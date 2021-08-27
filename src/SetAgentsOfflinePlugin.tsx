e25beec503e759d34459324cc80eb7a0b661e030
Bruno Kilian <bkilian@twilio.com>
2021-08-27 15:10:11 +0200

    cleanup

--- src/SetAgentsOfflinePlugin.tsx
+++ src/SetAgentsOfflinePlugin.tsx
@@ -2,45 +2,11 @@ import React from 'react';
 import * as Flex from '@twilio/flex-ui';
 import { FlexPlugin } from 'flex-plugin';
 
-import CustomTaskListContainer from './components/CustomTaskList/CustomTaskList.Container';
-import reducers, { namespace } from './states';
-
 const PLUGIN_NAME = 'SetAgentsOfflinePlugin';
-
 export default class SetAgentsOfflinePlugin extends FlexPlugin {
   constructor() {
     super(PLUGIN_NAME);
   }
 
-  /**
-   * This code is run when your plugin is being started
-   * Use this to modify any UI components or attach to the actions framework
-   *
-   * @param flex { typeof Flex }
-   * @param manager { Flex.Manager }
-   */
-  init(flex: typeof Flex, manager: Flex.Manager) {
-    this.registerReducers(manager);
-
-    const options: Flex.ContentFragmentProps = { sortOrder: -1 };
-    flex.AgentDesktopView
-      .Panel1
-      .Content
-      .add(<CustomTaskListContainer key="SetAgentsOfflinePlugin-component" />, options);
-  }
-
-  /**
-   * Registers the plugin reducers
-   *
-   * @param manager { Flex.Manager }
-   */
-  private registerReducers(manager: Flex.Manager) {
-    if (!manager.store.addReducer) {
-      // tslint: disable-next-line
-      console.error(`You need FlexUI > 1.9.0 to use built-in redux; you are currently on ${Flex.VERSION}`);
-      return;
-    }
-
-    manager.store.addReducer(namespace, reducers);
-  }
+  init(flex: typeof Flex) {}
 }
